f3c29720a0d1a13d0136e3d833a764720bbae4dd
EthanSeok <champ9162@naver.com>
2023-10-13 01:23:56 +0900

    streamlit 실행

--- app.py
+++ app.py
@@ -40,26 +40,26 @@ def isantichill(df, Tc):
         return (Tx - Tc)**2/(2*(Tx - Tn))
 
 
-def dvr_e(df):
-    C = 0.014
-    D = 0.062
-
+def dvr_e(df, C, D):
     if df['tavg'] >= 5:
         return C * np.exp(D * df['tavg'])
     else:
         return 0
 
 
-def dvr_model(df, Tc, Hr):
+def dvr_model(df, Tc, Hr, C, D):
     df = df[df['month'] >= 2]
     # df = df[df.index >= '01-30']
     df['Cdt'] = df.apply(lambda x: ischill(x, Tc[x['location']]), axis=1)
     df['Cat'] = df.apply(lambda x: isantichill(x, Tc[x['location']]), axis=1)
     df['Cd'] = abs(df['Cdt']).cumsum()
     df['Ca'] = abs(df['Cat']).cumsum()
-    print(df)
-    bloom = df[df['Ca'] >= Hr[df['location'][0]]].iloc[0].name.strftime('%m월%d일')
-    return bloom
+    df['DVRi'] = df.apply(lambda x: dvr_e(x, C[x['location']], D[x['location']]), axis=1)
+    df['DVR'] = df['DVRi'].cumsum()
+    # print(df)
+    bloom_chill = df[df['Ca'] >= Hr[df['location'][0]]].iloc[0].name.strftime('%m월%d일')
+    bloom_dvr = df[df['DVR'] >= 1].iloc[0].name.strftime('%m월%d일')
+    return bloom_chill, bloom_dvr
 
 
     # df['DVRt'] = df.apply(dvr_e, axis=1)
@@ -71,6 +71,8 @@ def main():
     select_year = st.number_input('연도 선택', min_value=2001, max_value=2023, value=2022)
     num_area = {'101': '춘천', '119': "수원", '131': '청주', '143': '대구', '156': "광주", '192': "진주"}
     area_num = {'춘천': '101', '수원': "119", '청주': '131', '대구': '143', '광주': "156", '진주': "192"}
+    C = {'101': 0.011, '119': 0.007, '131': 0.002, '143': 0.006, '156': 0.017, '192': 0.020}
+    D = {'101': 0.093, '119': 0.138, '131': 0.261, '143': 0.129, '156': 0.043, '192': 0.028}
     Tc = {'101': 5, '119': 6, '131': 7, '143': 5.2, '156': 8, '192': 5.1}
     Cr = {'101': -110, '119': -73, '131': -95, '143': -130, '156': -74, '192': -148}
     Hr = {'101': 245, '119': 180.2, '131': 199.2, '143': 277.4, '156': 150, '192': 271}
@@ -91,7 +93,22 @@ def main():
         df['location'] = str(location)
         df['DOY'] = df['date'].dt.dayofyear
         df.set_index('date', inplace=True)
-        st.write(f'{select_year}년 {loc}의 복숭아의 예상 개화일은 {dvr_model(df, Tc, Hr)}')
+        st.write(f'{select_year}년 {loc}의 복숭아의 예상 개화일은 {dvr_model(df, Tc, Hr, C, D)[0]}')
+
+    if st.button('DVR 모델 실행'):
+        st.write(f'{loc}지역의 {select_year}년의 복숭화 개화 모델 실행중...')
+        url = f'https://api.taegon.kr/station/{location}/?sy={year}&ey={year + year_for}&format=csv'
+        response = requests.get(url)
+        csv_data = response.content.decode('utf-8')
+        df = pd.read_csv(StringIO(csv_data), skipinitialspace=True)
+
+        df = df[df['year'] == select_year]
+        df = df[['year', 'month', 'day', 'tavg', 'tmax', 'tmin']]
+        df['date'] = pd.to_datetime(df[['year', 'month', 'day']])
+        df['location'] = str(location)
+        df['DOY'] = df['date'].dt.dayofyear
+        df.set_index('date', inplace=True)
+        st.write(f'{select_year}년 {loc}의 복숭아의 예상 개화일은 {dvr_model(df, Tc, Hr, C, D)[1]}')
 
 if __name__ == '__main__':
     main()
